18bc7e235646edb302b594ca074c4e40ecd1dc7b
Georgios Andreadis <info@gandreadis.com>
2020-06-20 11:51:40 +0200

    Add photograph

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,12 +6,17 @@ const Footer = ({ intl }) => (
     <p>
       {intl.formatMessage({ id: 'footer.initiators' })}: Hugo Pieters & Tjeerd
       Broerse
-    </p>
-    <p>
+      <br/>
       {intl.formatMessage({ id: 'footer.website' })}
       {': '}
       <a href="https://gandreadis.com" target="_blank" rel="noreferrer">
         Georgios Andreadis
+      </a>
+      <br/>
+      {intl.formatMessage({ id: 'footer.photographer' })}
+      {' '}
+      <a href="https://klari.net" target="_blank" rel="noreferrer">
+        Nic Limper
       </a>
     </p>
     <p>{intl.formatMessage({ id: 'footer.disclaimer' })}</p>
